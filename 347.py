835571a9d8b6c6125a432df36a829b32e5863fcd
Jinlin Song <songjinlin2010@163.com>
2018-04-02 20:09:24 -0700

    Create 347.py

---
+++ 347.py
@@ -0,0 +1,9 @@
+class Solution:
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        import collections
+        return list(dict(collections.Counter(nums).most_common(k)).keys())
