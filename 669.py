b57ada191b76bd7fee247fb98b29ad0cf9d39532
Jinlin Song <songjinlin2010@163.com>
2018-04-08 15:54:56 -0700

    Create 669.py

---
+++ 669.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# use the property of binary search tree: left nodes <= root and right nodes > root
+class Solution:
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        elif root.val > R:
+            return self.trimBST(root.left, L, R)
+        else:
+            root.right = self.trimBST(root.right, L, R)
+            root.left = self.trimBST(root.left, L, R)
+        return root
+        
+        
