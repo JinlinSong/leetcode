843545c6ff2ca7428e974de7194af336811b9cfd
Jinlin Song <10522939+JinlinSong@users.noreply.github.com>
2018-04-29 15:03:01 -0700

    Rename 16.py to 15.py

---
+++ 15.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        L = len(nums)
+        result = set()
+        for i in range(L - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            j, k = i+1, L-1
+            while j < k:
+                diff_temp = (nums[i] + nums[j] + nums[k])
+                if diff_temp == 0:
+                    result.add((nums[i], nums[j], nums[k]))
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    j += 1
+                elif diff_temp < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return [list(i) for i in result]
+
+def test_run():
+    a = Solution()
+    b = a.threeSum([-1, 0, 1, 2, -1, -4])
+    print(b)
+    
+if __name__ == "__main__":
+    test_run()
